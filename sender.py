d0f407772d7ca572f9b2920986acc9563d5452b1
Denis Matakov <denis@matakov.com>
2021-08-03 17:28:25 +0300

    Add check for broken token.

--- sender.py
+++ sender.py
@@ -14,11 +14,15 @@ async def chat_sender(host_, port_, uid, message=None):
     # Send uid to log in
     writer.write(f"{uid}\n".encode())
     user_info = await reader.readline()
-    user_info_dict = json.loads(user_info.decode())
-    logger.debug(user_info_dict["nickname"])
 
-    message = "Test message"
-    while True:
-        writer.write(f"{message}\n\n".encode())
-        await asyncio.sleep(1)
-    # writer.close()
+    user_info_dict = json.loads(user_info.decode())
+    if not user_info_dict:
+        logger.debug("Неизвестный токен. Проверьте его или зарегистрируйте заново.")
+    else:
+        logger.debug(user_info_dict)
+
+        message = "Test message"
+        while True:
+            writer.write(f"{message}\n\n".encode())
+            await asyncio.sleep(1)
+        # writer.close()
