8d22c7744a35ebac177f4d89284b609a261212c4
Denis Matakov <denis@matakov.com>
2021-08-03 14:52:24 +0300

    Brings CLI interface

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import asyncio
 import datetime
 import aiofiles
+import argparse
 
 
-async def chat_client():
-    reader, writer = await asyncio.open_connection("minechat.dvmn.org", 5000)
+async def chat_client(host_, port_, history_):
+    reader, writer = await asyncio.open_connection(host_, port_)
 
     while True:
         message = await reader.readline()
         timestamp = datetime.datetime.now().strftime("[%Y.%m.%d %H:%M:%S]")
         message_with_timestamp = f"{timestamp} {message.decode()}"
-        async with aiofiles.open("chat_logs.txt", "a") as chat_logs:
+        async with aiofiles.open(history_, "a") as chat_logs:
             await chat_logs.write(message_with_timestamp)
         print(message_with_timestamp)
 
@@ -18,4 +19,14 @@ async def chat_client():
 
 
 if __name__ == "__main__":
-    asyncio.run(chat_client())
+    parser = argparse.ArgumentParser(description='Connect to chat via socket.')
+    parser.add_argument("--host", help="Specify host to connect.")
+    parser.add_argument("--port", help="Specify port to connect.")
+    parser.add_argument("--history", help="Specify path to file to save logs.")
+
+    args = parser.parse_args()
+    host = args.host if args.host else "minechat.dvmn.org"
+    port = args.port if args.port else 5000
+    history = args.host if args.history else "chat_logs.txt"
+
+    asyncio.run(chat_client(host, port, history))
