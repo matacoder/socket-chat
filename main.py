d0ad17893fb9f2e61837236b4b3df65dd1a52b8a
Denis Matakov <denis@matakov.com>
2021-08-05 13:36:39 +0300

    Default arguments for reader.

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ from reader import chat_client_reader
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Connect to chat via socket.")
-    parser.add_argument("--host", help="Specify host to connect.")
-    parser.add_argument("--port", help="Specify port to connect.")
-    parser.add_argument("--history", help="Specify path to file to save logs.")
+    parser.add_argument("--host", help="Specify host to connect.", default="minechat.dvmn.org")
+    parser.add_argument("--port", help="Specify port to connect.", default=5000)
+    parser.add_argument("--logfile", help="Specify path to file to save logs.", default="chat_logs.txt")
 
     args = parser.parse_args()
-    host = args.host if args.host else "minechat.dvmn.org"
-    port = args.port if args.port else 5000
-    history = args.host if args.history else "chat_logs.txt"
 
-    asyncio.run(chat_client_reader(host, port, history))
+    asyncio.run(chat_client_reader(args.host, args.port, args.logfile))
