2cefc043bb90b75b15ac59d45bf527d963df3d8f
Denis Matakov <denis@matakov.com>
2021-08-11 16:18:49 +0300

    Delete comments

--- register.py
+++ register.py
@@ -13,22 +13,19 @@ def save_new_nickname(nick):
 
 
 if __name__ == "__main__":
-
     account_hash, nickname = load_from_dotenv()
     logger.debug(nickname)
-    # window
+
     root = Tk()
     root.geometry("400x150")
     root.title("Nickname saver.")
 
-    # username label and text entry box
     user_label = Label(root, text="Username").grid(row=0, column=0)
     username_tk_object = StringVar()
     if nickname:
         username_tk_object.set(nickname)
     username_entry = Entry(root, textvariable=username_tk_object).grid(row=0, column=1)
 
-    # password label and password entry box
     hash_label = Label(root, text="Account Hash").grid(row=1, column=0)
     hash_tk_object = StringVar()
     if account_hash:
@@ -39,7 +36,6 @@ if __name__ == "__main__":
 
     save_new_nickname = partial(save_new_nickname, username_tk_object)
 
-    # login button
     login_button = Button(
         root, text="Save new nickname", command=save_new_nickname
     ).grid(row=4, column=0)
