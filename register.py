a6b1a6e32379e5356f85f662e0abdc60d763db6c
Denis Matakov <denis@matakov.com>
2021-08-13 16:20:39 +0300

    Async register

--- register.py
+++ register.py
@@ -1,43 +1,80 @@
+import asyncio
 from tkinter import *
-from functools import partial
 
 from loguru import logger
 
+from gui import update_tk, TkAppClosed
+from helpers import load_config
+from loginer import register
 from sender import load_from_dotenv
 
 
+new_nickname = None
+
+
 def save_new_nickname(nick):
-    """Saving nickname. Registration would be at first app run."""
-    with open(".env", "w") as f:
-        f.write(f"nickname={nick.get()}")
+    global new_nickname
+    new_nickname = nick.get()
+    logger.debug(new_nickname)
 
 
-if __name__ == "__main__":
+async def register_new_nickname(user, hash):
+    settings = load_config()
+    global new_nickname
+    while True:
+        if new_nickname:
+            logger.debug(new_nickname)
+            nickname, account_hash = await register(
+                settings["host"],
+                settings["sender_port"],
+                new_nickname,
+                watchdog_queue=None,
+            )
+            new_nickname = None
+
+            user.set(nickname)
+            hash.set(account_hash)
+        await asyncio.sleep(0)
+
+
+async def create_window():
     account_hash, nickname = load_from_dotenv()
+    if not nickname:
+        nickname = "Anonymous"
     logger.debug(nickname)
 
     root = Tk()
     root.geometry("400x150")
     root.title("Nickname saver.")
 
-    user_label = Label(root, text="Username").grid(row=0, column=0)
+    Label(root, text="Username").grid(row=0, column=0)
     username_tk_object = StringVar()
     if nickname:
         username_tk_object.set(nickname)
-    username_entry = Entry(root, textvariable=username_tk_object).grid(row=0, column=1)
+    Entry(root, textvariable=username_tk_object).grid(row=0, column=1)
 
-    hash_label = Label(root, text="Account Hash").grid(row=1, column=0)
+    Label(root, text="Account Hash").grid(row=1, column=0)
     hash_tk_object = StringVar()
     if account_hash:
         hash_tk_object.set(account_hash)
-    password_entry = Entry(root, textvariable=hash_tk_object, state=DISABLED).grid(
-        row=1, column=1
-    )
+    Entry(root, textvariable=hash_tk_object, state=DISABLED).grid(row=1, column=1)
 
-    save_new_nickname = partial(save_new_nickname, username_tk_object)
-
-    login_button = Button(
-        root, text="Save new nickname", command=save_new_nickname
+    Button(
+        root,
+        text="Save new nickname",
+        command=lambda: save_new_nickname(username_tk_object),
     ).grid(row=4, column=0)
 
-    root.mainloop()
+    await asyncio.gather(
+        update_tk(root),
+        register_new_nickname(username_tk_object, hash_tk_object),
+    )
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(create_window())
+    except TkAppClosed:
+        pass
+    except KeyboardInterrupt:
+        pass
