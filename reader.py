cc6b5e89c8cb4a0bd0ef316db06364ad76cfc6bb
Denis Matakov <denis@matakov.com>
2021-08-16 12:39:53 +0300

    Finally block from review.

--- reader.py
+++ reader.py
@@ -42,35 +42,37 @@ async def chat_client_reader(
     port = settings["reader_port"]
     logger.debug(f"{host}:{port}")
     status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.INITIATED)
-    async with timeout(3):
-        try:
+
+    try:
+        async with timeout(3):
             reader, writer = await asyncio.open_connection(host, port)
-            status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.ESTABLISHED)
-        except socket.gaierror:
-            logger.debug("Reader gaierror")
+        status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.ESTABLISHED)
 
-    while True:
-        logger.debug(f"Reader loop \n{reader}\n{writer}")
+        while True:
+            logger.debug(f"Reader loop \n{reader}\n{writer}")
 
-        try:
-            message = await reader.readline()
-            if message:
-                watchdog_queue.put_nowait("Message have been read from server.")
-                current_formatted_datetime = datetime.datetime.now().strftime(
-                    "[%Y.%m.%d %H:%M:%S]"
-                )
-                message_with_datetime = (
-                    f"{current_formatted_datetime} {message.decode()}"
-                )
+            try:
+                message = await reader.readline()
+                if message:
+                    watchdog_queue.put_nowait("Message have been read from server.")
+                    current_formatted_datetime = datetime.datetime.now().strftime(
+                        "[%Y.%m.%d %H:%M:%S]"
+                    )
+                    message_with_datetime = (
+                        f"{current_formatted_datetime} {message.decode()}"
+                    )
 
-                messages_queue.put_nowait(message_with_datetime.rstrip())
-                log_queue.put_nowait(message_with_datetime.rstrip())
-        except asyncio.CancelledError:
-            writer.close()
-            await writer.wait_closed()
-            status_updates_queue.put_nowait(
-                gui.ReadConnectionStateChanged.CLOSED
-            )
-            break
+                    messages_queue.put_nowait(message_with_datetime.rstrip())
+                    log_queue.put_nowait(message_with_datetime.rstrip())
+            except asyncio.CancelledError:
+                break
 
-
+    except socket.gaierror:
+        logger.debug("Reader gaierror")
+    except asyncio.TimeoutError:
+        logger.debug("Reader failed on timeout")
+    finally:
+        logger.debug("Writer of reader closed!")
+        writer.close()
+        await writer.wait_closed()
+        status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.CLOSED)
